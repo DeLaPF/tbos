c81d3f40fa6c37d68bf67b092a16a58f3b1e72c8
DeLaPF <de.alafia@gmail.com>
2024-12-18 01:15:35 -0500

    skeleton for better handling multiple shaders

---
+++ src/shader_linker.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "shader.hpp"
+
+// TODO:
+// take in relative path to `shaderDir`
+// collect all files in `shaderDir`
+// pair `.vert` and `.frag` shader files with the same file name
+// for files with both vert and frag shaders, it should use `.shader` extension
+// for `.vert`/`.frag` files without a pair,
+// a `default.vert`/`default.frag` will be used to fill in for the missing file
+// if a file has no pair and no default is defined,
+// the error will be logged and that shader will not be included
+// if no shader programs can be compiled, the program will exit
+
+struct ShaderPair {
+    std::string name;
+    std::string vertPath;
+    std::string fragPath;
+};
+
+std::vector<ShaderPair> linkShaders(const char* shaderDir);
+std::vector<ParsedShader> compileShaders(const std::vector<ShaderPair>& shaderPairs);
