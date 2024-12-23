97d16cc067f65cacce0f408857fcfec67088a8ff
DeLaPF <de.alafia@gmail.com>
2024-12-22 22:26:51 -0500

    finally focusing reasonbly

--- res/shaders/06_6_focused_polar_hsb.frag.glsl
+++ res/shaders/06_6_focused_polar_hsb.frag.glsl
@@ -8,29 +8,26 @@ uniform vec2 u_Mouse;
 uniform vec2 u_Res;
 uniform float u_Time;
 
-float querp(float x, vec2 a, vec2 b, vec2 c, vec2 d)
+float terp(float x, vec2 a, vec2 b)
 {
-    float s0 = (b.y-a.y)/(b.x-a.x);
-    float s1 = (c.y-b.y)/(c.x-b.x);
-    float s2 = (d.y-c.y)/(d.x-c.x);
+    float s0 = a.y/a.x;
+    float s1 = (b.y-a.y)/(b.x-a.x);
 
-    float fx = (x*s0) + (a.y) - (a.x*s0);
-    float fbx = (b.x*s0) + (a.y) - (a.x*s0);
-    float gx = (x*s1) + (fbx) - (b.x*s1);
-    float gcx = (c.x*s1) + (fbx) - (b.x*s1);
-    float hx = (x*s2) + (gcx) - (c.x*s2);
+    float fx = x*s0;
+    float fax = a.x*s0;
+    float gx = (x*s1) + (fax) - (a.x*s1);
 
-    float selNone = 1.0-step(a.x, x) + step(d.x, x);
-    float selF = max(1.0-step(b.x, x) - selNone, 0.0);
-    float selG = max(1.0-step(c.x, x) - selF - selNone, 0.0);
-    float selH = max(1.0-step(d.x, x) - selG - selF - selNone, 0.0);
+    float selF = 1.0-step(a.x, x);
+    float selG = max(1.0-step(b.x, x) - selF, 0.0);
 
-    return (fx*selF) + (gx*selG) + (hx*selH);
+    return (fx*selF) + (gx*selG);
 }
 
-float querpP(float x, vec2 b, vec2 c)
+float terpP(float rad, float fWidth)
 {
-    return querp(x, vec2(0.0), b, c, vec2(PI*2.0, 1.0));
+    float w = min(fWidth, 0.99);
+    float x = mod(rad, PI*2.0);
+    return terp(x, vec2((PI*3.0)/2.0, w), vec2(PI*2.0, 1.0));
 }
 
 vec3 hsb2rgb(vec3 c)
@@ -57,23 +54,18 @@ float toNormRad(float degrees)
 void main()
 {
     vec2 st = gl_FragCoord.xy/u_Res;
-    // TODO: does not work above 90 due to wrapping around
-    // Need make everything fit and then rotate after...
-    float focusColor = 45; // does not support 0.0, 90.0
-    float focusSize = 5;
+    float focusColor = 180.0;
+    float focusWidth = 5.0;
 
     vec2 toCenter = vec2(0.5) - st;
     float angle = max(atan(toCenter.y, toCenter.x) + PI, 0.0);
     float radius = length(toCenter)*2.0;
-    float focusWedge0 = toRad(focusColor);
-    float focusWedge1 = toRad(focusColor+270);
-    float shapedAngle = querpP(
-        angle,
-        vec2(min(focusWedge0, focusWedge1), toNormRad(focusColor)),
-        vec2(max(focusWedge0, focusWedge1), toNormRad(focusColor+focusSize))
-    );
-    vec3 color = hsb2rgb(vec3(shapedAngle, radius, 1.0));
 
+    float transAngle = mod(angle-toNormRad(focusColor), PI*2.0);
+    float shapedAngle = terpP(transAngle, focusWidth);
+    float untransShaped = mod(shapedAngle+toNormRad(focusColor), 1.0);
+
+    vec3 color = hsb2rgb(vec3(untransShaped, radius, 1.0));
     color *= 1.0-step(0.5, length(toCenter)); // make circle
     gl_FragColor = vec4(color, 1.0);
 }
