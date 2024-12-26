e5fff5f6e812955a3885ced03310581f253ddc59
DeLaPF <de.alafia@gmail.com>
2024-12-25 21:54:27 -0500

    df exps

--- res/shaders/07_5_dist_field.frag.glsl
+++ res/shaders/07_5_dist_field.frag.glsl
@@ -15,11 +15,16 @@ void main()
 {
     vec2 st = gl_FragCoord.xy/u_Res;
     st = st*2.0 - 1.0;
-    // float df = length(abs(st)-0.3);
-    // float df = length(min(abs(st)-0.3, 0.0));
-    float df = length(max(abs(st)-0.3, 0.0));
 
-    vec3 color = vec3(fract(df*10.0));
+    float df = length(abs(st)-0.3);
+    df = length(min(abs(st)-0.3, 0.0));
+    df = length(max(abs(st)-0.3, 0.0));
 
+    float sel = fract(df * 10.0);
+    // sel = step(0.3, df);
+    // sel = step(0.3, df) * step(df, 0.4);
+    // sel = smoothstep(0.3, 0.4, df) * smoothstep(0.6, 0.5, df);
+
+    vec3 color = vec3(sel);
     gl_FragColor = vec4(color, 1.0);
 }
